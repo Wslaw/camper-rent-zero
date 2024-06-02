4171c839c60f653fdfefb7231e75b79679fb724c
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-06-02 12:01:02 +0300

    Update

--- src/components/FormField/FormField.jsx
+++ src/components/FormField/FormField.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import styles from "./formfield.module.css";
+import Icon from '../Icon/Icon';
+
 
 const FormField = () => {
   const [startDate, setStartDate] = useState(null);
@@ -13,27 +15,8 @@ const FormField = () => {
 
   const CustomInput = ({ value, onClick }) => (
     <div className={styles.inputGroup}>
-      <input
-        type="text"
-        className={styles.formControl}
-        onClick={onClick}
-        value={value}
-        placeholder="Booking date"
-        readOnly
-        aria-label="Booking date"
-      />
-      <svg onClick={handleIconClick} className={styles.icon} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M15.8333 3.33331H4.16667C3.24619 3.33331 2.5 4.07951 2.5 4.99998V16.6666C2.5 17.5871 3.24619 18.3333 4.16667 18.3333H15.8333C16.7538 18.3333 17.5 17.5871 17.5 16.6666V4.99998C17.5 4.07951 16.7538 3.33331 15.8333 3.33331Z"
-          stroke="#101828"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path d="M13.3335 1.66669V5.00002" stroke="#101828" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-        <path d="M6.6665 1.66669V5.00002" stroke="#101828" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-        <path d="M2.5 8.33331H17.5" stroke="#101828" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-      </svg>
+      <input type="text" className={styles.formControl} onClick={onClick} value={value} placeholder="Booking date" readOnly aria-label="Booking date" />
+      <Icon name="icon-calendar" className={styles.icon} onClick={handleIconClick} />
     </div>
   );
 
