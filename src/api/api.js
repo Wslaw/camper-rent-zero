311bdb83af35f038095f78953a6c31db203b1279
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-06-08 07:36:30 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,20 +2,20 @@ const URL = "https://665ecc2c1e9017dc16f17042.mockapi.io/api/";
 
 import axios from "axios";
 
-const booksInstance = axios.create({
+const camperInstance = axios.create({
   baseURL: "https://665ecc2c1e9017dc16f17042.mockapi.io/api/adverts",
 });
 
-export const requestFetchBooks = async () => {
-  const { data } = await booksInstance.get("/");
+export const requestFetchCampers = async () => {
+  const { data } = await camperInstance.get("/");
   return data;
 };
 
-export const requestAddBooks = async (body) => {
-  const { data } = await booksInstance.post("/", body);
+export const requestAddCampers = async (body) => {
+  const { data } = await camperInstance.post("/", body);
   return data;
 };
-export const requestDeleteBook = async (id) => {
-  const { data } = await booksInstance.delete(`/${id}`);
+export const requestDeleteCamper = async (id) => {
+  const { data } = await camperInstance.delete(`/${id}`);
   return data;
 };
