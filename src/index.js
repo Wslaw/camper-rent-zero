dd3d50c59276196f62bb550840b1d4bbf9ba63e5
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-06-11 12:24:34 +0300

    update nodemodule

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/campers-rent-one">
+    <BrowserRouter basename="/camper-rent-zero">
       <App />
     </BrowserRouter>
   </React.StrictMode>
