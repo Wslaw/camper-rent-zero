5fd59389c68adbaeecba9de7c5da83b96c02e9b8
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-06-11 12:44:56 +0300

    Updates

--- src/components/Icon/Icon.jsx
+++
@@ -1,8 +0,0 @@
-import sprite from "../../assets/svg/sprite.svg"
-
-const Icon = ({ name, width = 32, height = 32, className = "", onClick }) => (
-  <svg onClick={onClick} className={className} width={width} height={height}>
-    <use xlinkHref={`${sprite}#${name}`} />
-  </svg>
-);
-export default Icon;
